cf9bb8eea2f824e6f769e3484d5c40da8ee0b6bf
nashtodd <tatyygomezz@hotmail.com>
2026-06-14 16:35:50 -0600

    Se trabajó en la pestaña cuatro de la presentación, que es la creación del histograma de los gastos de los estudiantes según si son de universidad privada o pública, se podrá visualizar según los gastos de alimentación y alojamiento. Se realizó la parte del código tanto del UI como del server, para así mostrar un gráfico final.

--- Proyecto_Shiny/app.R
+++ Proyecto_Shiny/app.R
@@ -42,7 +42,31 @@ ui <- fluidPage(
                                "Privadas" = "Yes"),
                    selected = "Todas"),
       br()
-      )
+      ),
+      conditionalPanel(
+        condition = "input.pestanas == 'Gastos estudiantiles: Alojamiento y personales'",
+        helpText("Visualización de los datos estudiantiles según su tipo de universidad"),
+        br(),
+        selectInput(
+          inputId = "variable_costos",
+          label = "Seleccione algun tipo de gasto",
+          choices = c("Alojamiento" = "Room.Board", "Gastos personales" = "Personal"),
+          selected = "Room.Board"
+        ),
+        sliderInput(
+          inputId = "bins_costos",
+          label = "Cantidades",
+          min = 5,
+          max = 50,
+          value = 25
+        ),
+        checkboxInput(
+          inputId = "separar_tipo",
+          label = "Ver tipo de institución",
+          value = TRUE
+        )
+    ),
+    
     ),
     
     # Show a plot of the generated distribution
@@ -50,12 +74,15 @@ ui <- fluidPage(
       tabsetPanel(id = "pestanas",
         tabPanel("Análisis de matrícula"),
         tabPanel("Selectividad vs Graduación",
-      plotOutput("scatterPlot")),
+      plotOutput("scatterPlot")
+      ),
       tabPanel("tercera parte"),
-      tabPanel("cuarta parte")
+      tabPanel("Gastos estudiantiles: Alojamiento y personales",
+      plotOutput("histocostos")
       )
     )
   )
+)
 )
 
 # Define server logic required to draw a histogram
@@ -73,7 +100,7 @@ server <- function(input, output) {
     p <- ggplot(datos_filtrados, aes(x = aceptasa, y = Grad.Rate))
     
     if(input$tipo_universidad == "Todas"){
-      p <- p + geom_point(color ="steelblue", alpha = 0.6, size = 2) +
+      p <- p + geom_point(color ="slategray2", alpha = 0.6, size = 2) +
         labs(title = "Relación entre Tasa de Aceptación y Tasa de Graduación",
              x = "Tasa de Aceptación (%)",
              y = "Tasa de Graduación (%)")
@@ -86,7 +113,7 @@ server <- function(input, output) {
         color = "Tipo de Universidad"
       ) + 
       scale_colour_manual(
-        values = c("No" = "lightcoral", "Yes" = "palegreen3"),
+        values = c("No" = "coral3", "Yes" = "darkolivegreen2"),
         labels = c("No" = "Pública", "Yes" = "Privada"),
         drop = FALSE
       )}
@@ -99,6 +126,38 @@ server <- function(input, output) {
     #Mostrar el gráfico final
     p
   })
+  
+output$histocostos <- renderPlot({
+p_hist <- ggplot(datos, aes(x = .data[[input$variable_costos]]))
+
+if (input$separar_tipo){
+  p_hist <- p_hist + geom_histogram(
+    aes(fill = Private),
+    bins = input$bins_costos,
+    alpha = 0.6,
+    position = "identity"
+  ) + 
+    scale_fill_manual(
+      values = c("No" = "coral3", "Yes" = "darkolivegreen2"),
+      labels = c("No" = "Público", "Yes" = "Privado")
+    ) + 
+    labs(fill = "Tipo de Universidad")
+} else {
+  p_hist <- p_hist + geom_histogram(
+    bins = input$bins_costos,
+    fill = "slategray2",
+    color = "snow2",
+    alpha = 0.8
+  )
+}
+  
+p_hist + theme_minimal() +
+  labs(
+    title = paste("Distribución de gastos en", ifelse(input$variable_costos == "Room.Board", "Alojamiento", "Gastos personales")),
+    x = "Costo estimado en USD",
+    y = "Frecuencia"
+  )
+})
 }
 
 # Run the application 
